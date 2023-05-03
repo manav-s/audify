5564a9fd5ba439a3e85e53b4b1fde1e4a899c817
Manav Sharma <sharma.manav@northeastern.edu>
2023-05-03 11:18:32 -0400

    updated algorithm, added authorization for playlist mod support

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import SpotifyAuth from "./SpotifyAuth";
 import axios from "axios";
 
 const Home = () => {
@@ -8,6 +9,8 @@ const Home = () => {
   const [optimalPlaylist, setOptimalPlaylist] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loadingPhrase, setLoadingPhrase] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+  const [refreshToken, setRefreshToken] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,11 +20,9 @@ const Home = () => {
       return;
     }
 
-    setLoading(true);
-
     // Call your Flask backend API to get the optimized playlist
     try {
-      console.log(playlistLink);
+      setLoading(true);
       const response = await axios.post(
         "http://localhost:5000/optimize_playlist",
         {
@@ -35,6 +36,7 @@ const Home = () => {
     } catch (error) {
       console.error("Error fetching data:", error);
       setErrorOpen(true);
+      setLoading(false);
     }
   };
 
@@ -46,7 +48,6 @@ const Home = () => {
       return false;
     }
   }
-
 
   const loadingPhrases = [
     "Calculating song key compatibility...",
@@ -82,9 +83,80 @@ const Home = () => {
     setPlaylistLink("");
   };
 
+  const handleLogout = () => {
+    setAccessToken("");
+    setRefreshToken("");
+    window.location = "http://localhost:3000";
+  };
+
+  const modifyPlaylist = async () => {
+    try {
+      const playlistId = playlistLink;
+      const newUris = optimalPlaylist.map((song) => song.uri);
+
+      const response = await axios.post(
+        "http://localhost:5000/reorder_playlist",
+        {
+          playlist_id: playlistId,
+          new_uris: newUris,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      console.log(response.data);
+      alert("Playlist reordered successfully");
+    } catch (error) {
+      console.error("Error reordering playlist:", error);
+      alert("Error reordering playlist. Please try again. It may be a problem with your permissions!");
+    }
+  };
+
   const handleErrorClose = () => {
     setErrorOpen(false);
     setPlaylistLink("");
+  };
+
+  const isLoggedIn = () => {
+    return accessToken !== "";
+  };
+
+  const handleAuthCode = async (authCode) => {
+    try {
+      const response = await axios.post(
+        "https://accounts.spotify.com/api/token",
+        null,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization: `Basic ${btoa(
+              `3519692942004325a2c7160c90717ca5:9243be1df96e48bb829c4b07254bd82c`
+            )}`,
+          },
+          params: {
+            grant_type: "authorization_code",
+            code: authCode,
+            redirect_uri: "http://localhost:3000",
+          },
+        }
+      );
+
+      const { access_token, refresh_token } = response.data;
+      console.log("Access Token:", access_token);
+      console.log("Refresh Token:", refresh_token);
+
+      // Save the tokens in the state
+      setAccessToken(access_token);
+      setRefreshToken(refresh_token);
+    } catch (error) {
+      console.error(
+        "Error exchanging authorization code for access token:",
+        error
+      );
+    }
   };
 
   useEffect(() => {
@@ -99,112 +171,122 @@ const Home = () => {
     }
   }, [loading]);
 
-    return (
-      <>
-        <div
-          className={`container bg-gradient-to-r from-green-600 to-black shadow-lg max-w-full w-full mx-auto px-4 flex flex-col items-center justify-center min-h-screen text-white ${
-            loading ? "blur" : ""
-          }`}
-        >
-          <button className="fixed top-[5%] right-[10%] bg-white text-black font-bold py-2 px-6 rounded-full">
-            Log in
+  return (
+    <>
+      <div
+        className={`container bg-gradient-to-r from-green-600 to-black shadow-lg max-w-full w-full mx-auto px-4 flex flex-col items-center justify-center min-h-screen text-white ${
+          loading ? "blur" : ""
+        }`}
+      >
+        <SpotifyAuth
+          callback={(authCode) => handleAuthCode(authCode)}
+          loggedIn={isLoggedIn()}
+          handleLogout={handleLogout}
+        />
+
+        <h1 className="text-5xl font-bold mb-8 shadow-xl">Audify.</h1>
+        <form onSubmit={handleSubmit} className="w-full max-w-md">
+          <input
+            type="text"
+            className="block w-full py-2 px-4 mb-4 rounded border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-green-400 text-black"
+            placeholder="Playlist Link"
+            value={playlistLink}
+            onChange={(e) => setPlaylistLink(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="block w-full py-1 px-2 rounded bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400"
+          >
+            Optimize
           </button>
+        </form>
+      </div>
 
-          <h1 className="text-5xl font-bold mb-8 shadow-xl">Audify.</h1>
-          <form onSubmit={handleSubmit} className="w-full max-w-md">
-            <input
-              type="text"
-              className="block w-full py-2 px-4 mb-4 rounded border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-green-400 text-black"
-              placeholder="Playlist Link"
-              value={playlistLink}
-              onChange={(e) => setPlaylistLink(e.target.value)}
-            />
-            <button
-              type="submit"
-              className="block w-full py-1 px-2 rounded bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400"
-            >
-              Optimize
-            </button>
-          </form>
+      {loading && (
+        <div className="text-white fixed inset-0 flex items-center justify-center p-4 bg-black bg-opacity-50">
+          <div className="bg-gray-800 p-8 rounded-lg">
+            <p>{loadingPhrase}</p>
+          </div>
         </div>
+      )}
 
-        {loading && (
-          <div className="text-white fixed inset-0 flex items-center justify-center p-4 bg-black bg-opacity-50">
-            <div className="bg-gray-800 p-8 rounded-lg">
-              <p>{loadingPhrase}</p>
-            </div>
-          </div>
-        )}
-
-        {open && (
-          <div className="fixed text-white inset-0 flex items-center justify-center p-4 bg-black bg-opacity-50">
-            <div className="bg-gray-800 p-8 rounded-lg w-[85%]">
-              <h2 className="text-3xl font-bold mb-4">
-                Optimized Playlist Order
-              </h2>
-              <div className="overflow-x-auto overflow-y-auto h-96">
-                <table className="table-fixed w-full">
-                  <thead>
-                    <tr>
-                      <th className="w-[5%] text-left"></th>
-                      <th className="w-[10%] text-left"></th>
-                      <th className="w-[25%] text-left">Track Name</th>
-                      <th className="w-[15%] text-left">Artist</th>
-                      <th className="w-[20%] text-left">Album Name</th>
-                      <th className="w-[10%] text-left">Tempo (BPM)</th>
-                      <th className="w-[10%] text-left">Popularity</th>
+      {open && (
+        <div className="fixed text-white inset-0 flex items-center justify-center p-4 bg-black bg-opacity-50">
+          <div className="bg-gray-800 p-8 rounded-lg w-[85%]">
+            <h2 className="text-3xl font-bold mb-4">
+              Optimized Playlist Order
+            </h2>
+            <div className="overflow-x-auto overflow-y-auto h-96">
+              <table className="table-fixed w-full">
+                <thead>
+                  <tr>
+                    <th className="w-[5%] text-left"></th>
+                    <th className="w-[10%] text-left"></th>
+                    <th className="w-[25%] text-left">Track Name</th>
+                    <th className="w-[15%] text-left">Artist</th>
+                    <th className="w-[20%] text-left">Album Name</th>
+                    <th className="w-[10%] text-left">Tempo (BPM)</th>
+                    <th className="w-[10%] text-left">Popularity</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {optimalPlaylist.map((song, index) => (
+                    <tr key={song.position} className="mb-2">
+                      <td className="py-2">{index + 1}</td>
+                      <td>
+                        <img
+                          src={song.album_cover}
+                          alt={song.album_name}
+                          className="w-16 h-16"
+                        />
+                      </td>
+                      <td className="py-2">{song.track_name}</td>
+                      <td className="py-2">{song.artist}</td>
+                      <td className="py-2">{song.album_name}</td>
+                      <td className="py-2">{song.tempo}</td>
+                      <td className="py-2">{song.popularity}</td>
                     </tr>
-                  </thead>
-                  <tbody>
-                    {optimalPlaylist.map((song, index) => (
-                      <tr key={song.position} className="mb-2">
-                        <td className="py-2">{index + 1}</td>
-                        <td>
-                          <img
-                            src={song.album_cover}
-                            alt={song.album_name}
-                            className="w-16 h-16"
-                          />
-                        </td>
-                        <td className="py-2">{song.track_name}</td>
-                        <td className="py-2">{song.artist}</td>
-                        <td className="py-2">{song.album_name}</td>
-                        <td className="py-2">{song.tempo}</td>
-                        <td className="py-2">{song.popularity}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-              <button
-                className="block mt-4 ml-auto py-1 px-2 rounded bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400"
-                onClick={handleClose}
-              >
-                Close
-              </button>
+                  ))}
+                </tbody>
+              </table>
             </div>
+            {isLoggedIn() && (
+              <button
+                className="block mt-4 text-black ml-auto py-1 px-4 rounded-full bg-white hover:bg-slate-200 focus:outline-none focus:ring-2 focus:ring-green-400"
+                onClick={modifyPlaylist}
+              >
+                Modify current playlist ordering
+              </button>
+            )}
+            <button
+              className="block mt-4 ml-auto py-1 px-4 rounded-full bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400"
+              onClick={handleClose}
+            >
+              Close
+            </button>
           </div>
-        )}
+        </div>
+      )}
 
-        {errorOpen && (
-          <div className="fixed inset-0 text-white flex items-center justify-center p-4 bg-black bg-opacity-50">
-            <div className="bg-gray-800 p-8 rounded-lg">
-              <h2 className="text-3xl font-bold mb-4">Error</h2>
-              <p>
-                Try a different playlist. Please make sure that the playlist is
-                public!
-              </p>
-              <button
-                className="block mt-4 ml-auto py-1 px-2 rounded bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400"
-                onClick={handleErrorClose}
-              >
-                X
-              </button>
-            </div>
+      {errorOpen && (
+        <div className="fixed inset-0 text-white flex items-center justify-center p-4 bg-black bg-opacity-50">
+          <div className="bg-gray-800 p-8 rounded-lg">
+            <h2 className="text-3xl font-bold mb-4">Error</h2>
+            <p>
+              Try a different playlist. Please make sure that the playlist is
+              public!
+            </p>
+            <button
+              className="block mt-4 ml-auto py-1 px-2 rounded bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400"
+              onClick={handleErrorClose}
+            >
+              X
+            </button>
           </div>
-        )}
-      </>
-    );
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Home;
---
+++ src/components/OptimizedPlaylist.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OptimizedPlaylist = () => {
+  return (
+    <div>OptimizedPlaylist</div>
+  )
+}
+
+export default OptimizedPlaylist
